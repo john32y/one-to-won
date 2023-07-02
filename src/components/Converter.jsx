a2b49705d756633ed13a39306ba470e81ef7e07a
john32y <john.thelronge@gmail.com>
2023-07-02 17:15:58 -0400

    Updated converter

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
-
-export default function Converter (){
-    const [money, setMoney]= useState(0)
-
-   const value = (e) => {
-            e.preventDefault();
-            const converting = e.target.input.value 
-            const result= (converting * 1318.11)
-            setMoney(result);
+export default function Converter() {
+    const [money, setMoney] = useState(0)
+    const [currency, setCurrency] = useState("USD")
+    const [currencytwo, setCurrencytwo] = useState("Won")
+    const convertionTable = {
+        Won: 1318.11
+    }
+    const findingBase = {
+        Won: 0.00076
+    }
+    function swap (){
+        const mon1 = currencytwo;
+        setCurrencytwo(currency)
+        setCurrency(mon1)
+        console.log(currency + " " + currencytwo)
+    }
+    function inputCurrency (){
+    }
+    const value = (e) => {
+        e.preventDefault();
+        let result = 0;
+        const converting = e.target.input.value
+        const namemon = currency
+        result = (converting * (convertionTable.Won))
+        if(currency !== "USD"){
+            let math1 = findingBase[currency]
+            console.log(currency)
+            console.log(math1)
+            result = (converting * math1)
         }
-        return(
+        setMoney(result);
+    }
+    return (
         <section>
             <form onSubmit={value}>
-            <h1>USD to Won</h1>
-            <input type="text" name="input" />
-            <input type="submit" value="convert" />
-            <p>₩ {money}</p>
+                <h2>{currency} to {currencytwo}</h2>
+                <input type="text" name="input" />
+        {/* <select onChange={inputCurrency}>
+       <option value="bitcoin">BitCoin</option>
+       <option value="won">Won</option>
+       <option value="dollar">Dollars</option>
+     </select> */}
+                <input type="submit" value="convert" />
+                <button onClick={swap}>:arrows_counterclockwise:</button>
+                <p>{money} {currencytwo}</p>
             </form>
         </section>
     )
